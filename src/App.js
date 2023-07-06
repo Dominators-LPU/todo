04c034b98e7d833eb0d8cd6b16fcf652314d9f8a
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-07-06 10:37:12 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todoArray, setTodoArray] = useState([]);
+
+  console.log(todo);
+  console.log(todoArray);
+
+  function addToDo(e){
+    e.preventDefault();
+    setTodoArray([...todoArray, todo])
+    setTodo("");
+
+  }
+
+  function deleteTodo (item){
+        setTodoArray(todoArray.filter((todo) => todo !== item))
+  }
+
+
+  return(
+    <div>
+              <form>
+                     <input type="text" placeholder="Enter your todo"
+                      onChange={e => setTodo(e.target.value)}
+                      value={todo}
+                     />
+                      <button type="submit" onClick={addToDo}>Add</button>
+              </form>
+              <ul> 
+                  {
+                    todoArray.length >0 && (
+                      todoArray.map((item) => (
+                           <li>
+                              <span>{item}</span>
+                              <button onClick={()=>deleteTodo(item)}>Delete</button>
+                           </li>
+                      ))
+                    )
+                  }
+
+              </ul>
     </div>
-  );
+  )
 }
 
 export default App;
